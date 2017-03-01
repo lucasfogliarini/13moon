bc0de9e6c6775df595e25f65dd6bb6a2f272f0a2
Lucas Fogliarini <lucasfogliarini@gmail.com>
2017-03-01 17:59:53 -0300

    apply images

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 function changeDate(){
     var kin = new Kin(day.value, month.value, year.value);
     kin_element.innerHTML = kin.kin.mantra + ' (' + kin.kin.number + ')';
+    tone.src = 'tones/'+ kin.kin.tone.number + '.png';
+    seal.src = 'seals/'+ kin.kin.seal.number + '.png';
+
     var description = kin.generateDescription(kin.kin.seal.number, kin.kin.tone.number);
     description_element.innerHTML = '';
     for (var i = 0; i < description.length; i++) {
@@ -13,6 +16,8 @@ var day = document.getElementById('d');
 var month = document.getElementById('m');
 var year = document.getElementById('y');
 var kin_element = document.getElementById('kin');
+var tone = document.getElementById('tone');
+var seal = document.getElementById('seal');
 var description_element = document.getElementById('description');
 day.addEventListener("input", changeDate);
 month.addEventListener("input", changeDate);
