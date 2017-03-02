4e3aae9254604ecb3a6f08af8521c1edb32da25c
Lucas Fogliarini <lucasfogliarini@gmail.com>
2017-03-02 10:34:08 -0300

    get image reference from lawoftime.org

--- js/main.js
+++ js/main.js
@@ -4,32 +4,29 @@ function changeDate(){
       return;
     }
     var kin = new Kin(day.value, month.value, year.value);
-    kin_element.innerHTML = kin.kin.mantra + ' (' + kin.kin.number + ')';
-    tone.src = 'tones/'+ kin.kin.tone.number + '.png';
-    seal.src = 'seals/'+ kin.kin.seal.number + '.png';
+    kin_title.innerHTML = kin.kin.mantra + ' (' + kin.kin.number + ')';
+    kin_image.src = 'http://www.lawoftime.org/decode/sealstones/kin/'+kin.kin.number +'.gif';
 
     var description = kin.generateDescription(kin.kin.seal.number, kin.kin.tone.number);
-    description_element.innerHTML = '';
+    kin_description.innerHTML = '';
     for (var i = 0; i < description.length; i++) {
       var p = "<p>" + description[i] + "</p>";
-      description_element.insertAdjacentHTML('beforeend', p);
+      kin_description.insertAdjacentHTML('beforeend', p);
     }
 }
 
 function clear(){
-   kin_element.innerHTML = '';
-   tone.src = '';
-   seal.src = '';
-   description_element.innerHTML = '';
+   kin_title.innerHTML = '';
+   kin_image.src = '';
+   kin_description.innerHTML = '';
 }
 
 var day = document.getElementById('d');
 var month = document.getElementById('m');
 var year = document.getElementById('y');
-var kin_element = document.getElementById('kin');
-var tone = document.getElementById('tone');
-var seal = document.getElementById('seal');
-var description_element = document.getElementById('description');
+var kin_title = document.getElementById('kin_title');
+var kin_image = document.getElementById('kin_image');
+var kin_description = document.getElementById('kin_description');
 day.addEventListener("input", changeDate);
 month.addEventListener("input", changeDate);
 year.addEventListener("input", changeDate);
